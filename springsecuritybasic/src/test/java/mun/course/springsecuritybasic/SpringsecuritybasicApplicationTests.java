0205a2506f398ed6dd2f40e5245da326fda05bb8
Alexandr Mun <mun_a2006@yahoo.de>
2023-11-20 09:06:46 +0600

    initial commit

---
+++ springsecuritybasic/src/test/java/mun/course/springsecuritybasic/SpringsecuritybasicApplicationTests.java
@@ -0,0 +1,13 @@
+package mun.course.springsecuritybasic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringsecuritybasicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
