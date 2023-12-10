344898f198ab714be08f79e8b0a8f82f6ab09c00
Alexandr Mun <mun_a2006@yahoo.de>
2023-12-10 11:37:46 +0600

    added custom authentication provider

---
+++ springsecsection5/src/main/java/mun/course/controller/LoginController.java
@@ -0,0 +1,42 @@
+package mun.course.controller;
+
+import io.swagger.v3.oas.models.annotations.OpenAPI30;
+import mun.course.model.Customer;
+import mun.course.repository.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LoginController {
+
+    @Autowired
+    private CustomerRepository customerRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @PostMapping("/register")
+    public ResponseEntity<String> registerUser(@RequestBody Customer customer) {
+        ResponseEntity response = null;
+        try {
+            String hashPassword = passwordEncoder.encode(customer.getPwd());
+            customer.setPwd(hashPassword);
+            Customer savedCustomer = customerRepository.save(customer);
+            if (savedCustomer.getId() > 0) {
+                response = ResponseEntity
+                        .status(HttpStatus.CREATED)
+                        .body("Given user details are successfully registered");
+            }
+        } catch (Exception ex) {
+            response = ResponseEntity
+                    .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body("An exception occurred due to " + ex.getMessage());
+        }
+        return response;
+    }
+}
