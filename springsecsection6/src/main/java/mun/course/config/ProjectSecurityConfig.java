6ee8655a1599ce401b0be6d5ce7776b52c13697c
Alexandr Mun <mun_a2006@yahoo.de>
2024-01-26 07:18:15 +0600

    SSEC-003 added csrf configuration

--- springsecsection6/src/main/java/mun/course/config/ProjectSecurityConfig.java
+++ springsecsection6/src/main/java/mun/course/config/ProjectSecurityConfig.java
@@ -1,13 +1,20 @@
 package mun.course.config;
 
+import mun.course.filter.CsrfCookieFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.security.web.csrf.CsrfTokenRequestAttributeHandler;
+import org.springframework.web.cors.CorsConfiguration;
 
-import static org.springframework.security.config.Customizer.withDefaults;
+import java.util.Collections;
 
 @Configuration
 public class ProjectSecurityConfig {
@@ -30,17 +37,37 @@ public class ProjectSecurityConfig {
 
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
-        http.csrf().disable(); //временно
 
-        http.authorizeHttpRequests((requests) ->
-                requests.requestMatchers(
-                        AUTH_WHITELIST)
-                        .authenticated());
+        CsrfTokenRequestAttributeHandler requestHandler = new CsrfTokenRequestAttributeHandler();
+        requestHandler.setCsrfRequestAttributeName("_csrf");
 
-        http.authorizeHttpRequests((requests) -> requests.requestMatchers("/notices", "/contact", "/register").permitAll());
+        http.securityContext((context) -> context
+                        .requireExplicitSave(false))
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.ALWAYS))
 
-        http.formLogin(withDefaults());
-        http.httpBasic(withDefaults());
+                .cors(corsCustomizer -> corsCustomizer.configurationSource(request -> {
+                    CorsConfiguration config = new CorsConfiguration();
+                    config.setAllowedOrigins(Collections.singletonList("http://localhost:4200"));
+                    config.setAllowedMethods(Collections.singletonList("*"));
+                    config.setAllowCredentials(true);
+                    config.setAllowedHeaders(Collections.singletonList("*"));
+                    config.setMaxAge(3600L);
+                    return config;
+                }))
+
+                .csrf(csrf -> csrf.csrfTokenRequestHandler(requestHandler)
+                        .ignoringRequestMatchers("/contact", "/register")
+                        .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()))
+
+                .addFilterAfter(new CsrfCookieFilter(), BasicAuthenticationFilter.class)
+
+                .authorizeHttpRequests((requests) -> requests
+                        .requestMatchers(AUTH_WHITELIST).authenticated()
+                        .requestMatchers("/notices", "/contact", "/register").permitAll())
+
+                .formLogin(Customizer.withDefaults())
+                .httpBasic(Customizer.withDefaults());
+
         return http.build();
     }
 
